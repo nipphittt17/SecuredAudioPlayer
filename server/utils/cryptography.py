e007b75bc73fc74539bfea7cb11ada83d967becd
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-03-25 00:37:44 +0700

    docs: todos 🧠

--- server/utils/cryptography.py
+++ server/utils/cryptography.py
@@ -10,7 +10,7 @@ class AES:
 
     @staticmethod
     def decrypt_audio(encrypted_audio_file_base64: str, secret_key: str) -> str:
-        # Add the logic here
+        # TODO: Add the logic here
         # This is just a placeholder, replace with correct logic
 
         decrypted_audio_file_base64 = encrypted_audio_file_base64 + secret_key
@@ -18,7 +18,7 @@ class AES:
 
     @staticmethod
     def encrypt_audio(raw_audio_file_base64: str, secret_key: str) -> str:
-        # Add the logic here
+        # TODO: Add the logic here
         # This is just a placeholder, replace with correct logic
 
         encrypted_audio_file_base64 = raw_audio_file_base64 + secret_key
