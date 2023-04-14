d3ffc346387513b44f0ba4e6bb28dffa952d33cc
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2023-04-14 17:41:41 +0700

    encrypt+decrypt

---
+++ server/test.py
@@ -0,0 +1,53 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+def pad_key(key):
+    valid_key_sizes = [16, 24, 32]
+    key_length = len(key)
+    if key_length in valid_key_sizes:
+        return key
+    elif key_length < valid_key_sizes[0]:
+        return key + b'\0' * (valid_key_sizes[0] - key_length)
+    elif key_length < valid_key_sizes[1]:
+        return key + b'\0' * (valid_key_sizes[1] - key_length)
+    else:
+        return key + b'\0' * (valid_key_sizes[2] - key_length)
+
+# Generate a random 256-bit key and IV
+key = os.urandom(32)
+iv = os.urandom(16)
+
+# Pad the key to the valid length
+key = pad_key(key)
+
+# Define the plaintext to be encrypted
+plaintext = "Hello, world!"
+
+# Create an AES cipher with CBC mode and the specified key and IV
+backend = default_backend()
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+
+# Pad the input string to a multiple of the block size
+block_size = algorithms.AES.block_size // 8
+padding_length = block_size - len(plaintext) % block_size
+padded_plaintext = plaintext.encode() + bytes([padding_length] * padding_length)
+
+# Encrypt the padded plaintext
+encryptor = cipher.encryptor()
+ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
+
+# Decrypt the ciphertext
+decryptor = cipher.decryptor()
+padded_decrypted_text = decryptor.update(ciphertext) + decryptor.finalize()
+
+# Remove the padding from the decrypted text
+padding_length = padded_decrypted_text[-1]
+decrypted_text = padded_decrypted_text[:-padding_length].decode()
+
+# Print the results
+print("Key:", key)
+print("IV:", iv)
+print("Plaintext:", plaintext)
+print("Ciphertext:", ciphertext)
+print("Decrypted text:", decrypted_text)
--- server/utils/cryptography.py
+++ server/utils/cryptography.py
@@ -1,26 +1,61 @@
 from utils.generator import random_string
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+import base64
+from base64 import b64encode
 
 
 class AES:
 
-    @staticmethod
     def generate_secret_key() -> str:
-        LENGTH_FOR_SECRET_KEY = 44
+        LENGTH_FOR_SECRET_KEY = 32
+        # return os.urandom(LENGTH_FOR_SECRET_KEY)
         return random_string(LENGTH_FOR_SECRET_KEY)
+    
 
     @staticmethod
     def decrypt_audio(encrypted_audio_file_base64: str, secret_key: str) -> str:
-        # TODO: Add the logic here
-        # This is just a placeholder, replace with correct logic
+        backend = default_backend()
+        encrypted_data = base64.b64decode(encrypted_audio_file_base64)
 
-        decrypted_audio_file_base64 = encrypted_audio_file_base64.removesuffix(
-            secret_key)
-        return decrypted_audio_file_base64
+        # Split the encrypted data into the IV and the encrypted audio file
+        iv = encrypted_data[:16]
+        secret_key_bytes = secret_key.encode()
+        cipher = Cipher(algorithms.AES(secret_key_bytes), modes.CBC(iv), backend=backend)
+
+        encrypted_audio_file = encrypted_data[16:]
+        # Decrypt the ciphertext
+        decryptor = cipher.decryptor()
+        decrypted_padded_plaintext = decryptor.update(encrypted_audio_file) + decryptor.finalize()
+
+        # Remove the padding from the decrypted plaintext
+        padding_length = decrypted_padded_plaintext[-1]
+        decrypted_audio_file_base64 = decrypted_padded_plaintext[:-padding_length]
+        return decrypted_audio_file_base64.decode()
 
     @staticmethod
     def encrypt_audio(raw_audio_file_base64: str, secret_key: str) -> str:
-        # TODO: Add the logic here
-        # This is just a placeholder, replace with correct logic
+        raw_audio_file_bytes = raw_audio_file_base64.encode()
+        secret_key_bytes = secret_key.encode()
 
-        encrypted_audio_file_base64 = raw_audio_file_base64 + secret_key
+        # Generate a random initialization vector
+        iv = os.urandom(16)
+
+        # Create an AES cipher with CBC mode and the specified key and IV
+        cipher = Cipher(algorithms.AES(secret_key_bytes), modes.CBC(iv), backend=default_backend())
+
+        # Pad the plaintext to a multiple of the block size
+        block_size = algorithms.AES.block_size // 8
+        padding_length = block_size - len(raw_audio_file_bytes) % block_size
+        padded_plaintext = raw_audio_file_bytes + bytes([padding_length] * padding_length)
+
+        # Encrypt the padded plaintext
+        encryptor = cipher.encryptor()
+        encrypted_audio_file = encryptor.update(padded_plaintext) + encryptor.finalize()
+
+        # Concatenate the IV and the encrypted audio file, and return the result as a base64-encoded string
+        encrypted_data = iv + encrypted_audio_file
+        encrypted_audio_file_base64 = b64encode(encrypted_data).decode()
         return encrypted_audio_file_base64
+  
