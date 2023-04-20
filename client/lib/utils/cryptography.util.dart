6cb744400e85cbbd30a749b67ef1c0ab559e51d9
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-04-20 12:34:44 +0700

    chore: remove unused codes 🔥

--- client/lib/utils/cryptography.util.dart
+++ client/lib/utils/cryptography.util.dart
@@ -1,17 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:crypto/crypto.dart';
-
 class CryptographyUtil {
   static Future<String> convertToBase64(File file) async {
     final bytes = await file.readAsBytes();
     return base64.encode(bytes);
-  }
-
-  static String sha256Hashing(String target) {
-    List<int> bytes = utf8.encode(target);
-    Digest hash = sha256.convert(bytes);
-    return hash.toString();
   }
 }
